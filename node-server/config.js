2eb967cb94d79086791c6ce6e5bfee0cf99c7f51
sia_02 <53578618+siajin@users.noreply.github.com>
2023-03-02 13:49:29 +0900

    Update config.js

--- node-server/config.js
+++ node-server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    "mongodb+srv://siajin1:shi1528624@cluster0.k73o1qh.mongodb.net/semteul?retryWrites=true&w=majority",
+    "mongodb+srv://siajin1:userPassword@cluster0.k73o1qh.mongodb.net/semteul?retryWrites=true&w=majority",
   SECRET_KEY: "ASDF",
 };
